3034581ff6cfc2f9a5a85a01dc0aaeb0370a19ae
gimjay23 <kojoasumani@hotmail.com>
2020-10-08 19:39:37 +0000

    a few background tweaks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./App.css";
-import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+
+//Imported Libraries
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//imported components
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="main">
-      <div>
-
-        <Navbar className="nav-bar" variant="dark">
-          <Navbar.Brand href="#home"><img src="https://img.icons8.com/offices/40/000000/partly-cloudy-rain.png" alt="" /></Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features" title="Sign in to view you location's weather data">Sign In</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-        </Navbar>
-      </div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route>
+            <Route exact path="/" component={Home} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/about" component={About} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SignIn() {
+  return (
+    <div className="weather-page">
+      <center>
+        <br/>
+        <h3>Welcome to THE WEATHER APP</h3>
+        <hr style={{width:"40%"}}/>
+
+      </center>
+    </div>
+  );
+};
