13421fcd8f1102a1da7e8ca5b5d0dfc3bfb70ca8
gimjay23 <kojoasumani@hotmail.com>
2020-10-09 00:56:53 +0000

    minor tweaks; about page rerender

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,43 @@
 import React from "react";
 import NavBar from "./NavBar";
-import Storm from "./Storm";
+import { Container, Row, Col } from "react-bootstrap";
+import storm from "./storm.png";
 
 export default function About() {
+  const widgetStorm = {
+    backgroundColor: "inherit",
+    marginTop: "50px",
+    height: "200px",
+    textAlign: "center",
+    padding: "10px",
+  };
+
+  const myText = (
+    <Container fluid>
+      <Row>
+        <Col md={6}>
+          <div style={widgetStorm}>
+            <img src={storm} alt="stormy weather" />
+          </div>
+        </Col>
+        <Col md={6}>
+          <div style={widgetStorm}>
+            <h2 style={{fontFamily: "candara"}} className="h2-font">THE WEATHER APP</h2>
+            <p style={{color: "black"}} className="p-font">Developed by,</p>
+            <p style={{color: "black"}} className="p-font">
+              © 2020 Copyright • <a href="https://www.instagram.com/kdworks/">KD WORKS LTD.</a>
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+
   return (
     <div className="about-page">
       <NavBar />
       <br />
-      <Storm />
+      {myText}
     </div>
   );
 }
--- src/components/Storm.js
+++ src/components/Storm.js
@@ -4,7 +4,7 @@ import storm from "./storm.png";
 export default function Storm() {
   const widgetStorm = {
     backgroundColor: "inherit",
-    margin: "0 0 0 500px",
+
     height: "auto",
     padding: "30px",
   };
